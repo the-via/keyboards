09f01a00f9a08c891d6cfbb603e38ada6f884e65
Olivia Briggs <ofbriggs@gmail.com>
2019-12-13 19:34:14 -0500

    Add V2 Definitions

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,21 +3,38 @@ import * as glob from 'glob';
 import * as fs from 'fs';
 import * as rimraf from 'rimraf';
 import {promisify} from 'bluebird';
-import {generateVIADefinitionLookupMap} from 'via-reader';
+import {
+  generateVIADefinitionLookupMap,
+  generateVIADefinitionV2LookupMap
+} from 'via-reader';
 
-const viaAPIVersion = '0.0.1';
+const viaAPIVersionV1 = '0.0.1';
+const viaAPIVersionV2 = '0.1.1';
 
 async function build() {
   await promisify(rimraf)('dist/*');
 
-  const definitions = glob
-    .sync('src/**/*.json', {absolute: true})
-    .map(f => require(f));
+  const paths = glob.sync('src/**/*.json', {absolute: true});
+  const fileIsV2 = (path: string) => path.indexOf('v2.json') !== -1;
+  const [v1Paths, v2Paths] = [
+    paths.filter(path => !fileIsV2(path)),
+    paths.filter(fileIsV2)
+  ];
 
-  const res = {
-    version: viaAPIVersion,
+  const [v1Definitions, v2Definitions] = [v1Paths, v2Paths].map(paths =>
+    paths.map(f => require(f))
+  );
+
+  const resV1 = {
+    version: viaAPIVersionV1,
     generatedAt: Date.now(),
-    definitions: generateVIADefinitionLookupMap(definitions)
+    definitions: generateVIADefinitionLookupMap(v1Definitions)
+  };
+
+  const resV2 = {
+    version: viaAPIVersionV2,
+    generatedAt: Date.now(),
+    definitions: generateVIADefinitionV2LookupMap(v2Definitions)
   };
 
   try {
@@ -25,7 +42,8 @@ async function build() {
       fs.mkdirSync('dist');
     }
 
-    fs.writeFileSync('dist/keyboards.json', stringify(res));
+    fs.writeFileSync('dist/keyboards.json', stringify(resV1));
+    fs.writeFileSync('dist/keyboards.v2.json', stringify(resV2));
   } catch (error) {
     console.error(error);
     process.exit(1);
