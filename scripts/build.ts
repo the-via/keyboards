b96f1b97848a5443600a35ebed1e9e802ea30293
Olivia Briggs <ofbriggs@gmail.com>
2019-12-24 05:07:01 -0500

    Delete v1 definitions and only generate v2

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,33 +3,16 @@ import * as glob from 'glob';
 import * as fs from 'fs';
 import * as rimraf from 'rimraf';
 import {promisify} from 'bluebird';
-import {
-  generateVIADefinitionLookupMap,
-  generateVIADefinitionV2LookupMap
-} from 'via-reader';
+import {generateVIADefinitionV2LookupMap} from 'via-reader';
 
-const viaAPIVersionV1 = '0.0.1';
 const viaAPIVersionV2 = '0.1.1';
 
 async function build() {
   await promisify(rimraf)('dist/*');
 
   const paths = glob.sync('src/**/*.json', {absolute: true});
-  const fileIsV2 = (path: string) => path.indexOf('v2.json') !== -1;
-  const [v1Paths, v2Paths] = [
-    paths.filter(path => !fileIsV2(path)),
-    paths.filter(fileIsV2)
-  ];
 
-  const [v1Definitions, v2Definitions] = [v1Paths, v2Paths].map(paths =>
-    paths.map(f => require(f))
-  );
-
-  const resV1 = {
-    version: viaAPIVersionV1,
-    generatedAt: Date.now(),
-    definitions: generateVIADefinitionLookupMap(v1Definitions)
-  };
+  const [v2Definitions] = [paths].map(paths => paths.map(f => require(f)));
 
   const resV2 = {
     version: viaAPIVersionV2,
@@ -42,7 +25,6 @@ async function build() {
       fs.mkdirSync('dist');
     }
 
-    fs.writeFileSync('dist/keyboards.json', stringify(resV1));
     fs.writeFileSync('dist/keyboards.v2.json', stringify(resV2));
   } catch (error) {
     console.error(error);
