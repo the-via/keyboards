0a4aaf4a83eff556ff72d67306b7962c6fc41146
Olivia Briggs <ofbriggs@gmail.com>
2020-01-01 18:40:05 -0500

    Add VIA website template

---
+++ caniusevia/docusaurus.config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  title: 'VIA',
+  tagline: 'Complete customization made trivial',
+  url: 'https://caniusevia.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'the-via', // Usually your GitHub org/user name.
+  projectName: 'keyboards', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'VIA',
+      logo: {
+        alt: 'VIA',
+        src: 'img/icon.png'
+      },
+      links: [{to: 'docs/doc1', label: 'Docs', position: 'left'}]
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Docs',
+              to: 'docs/doc1'
+            }
+          ]
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus'
+            }
+          ]
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog'
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus'
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus'
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+    }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      }
+    ]
+  ]
+};
