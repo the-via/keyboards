d4dd433ea601ad5698df62ea3f49b5cd0161de30
ValdisThomann <valdis.thomann@gmail.com>
2020-01-08 02:21:39 +1100

    firmware build script

---
+++ scripts/build-firmware.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+build_dir=$(pwd)
+
+# Make sure qmk is installed
+git -C ~/qmk git pull || git clone https://github.com/qmk/qmk_firmware.git ~/qmk
+cd ~/qmk
+util/qmk_install.sh
+make git-submodule
+
+make all:via
+
+# copy hex and bin files to dist/firmware
+target_dir="$build_dir/dist/firmware"
+mkdir -p $target_dir
+find -maxdepth 1 -regex '.*\.\(hex\|bin\)' -exec mv -f {} $target_dir \;
