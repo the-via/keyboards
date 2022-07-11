9e64cf08f1d455d750d4626b480ccffc342ff08a
Olivia Briggs <ofbriggs@gmail.com>
2022-07-11 00:03:54 -0400

    Show duplicate ids when building definitions

--- scripts/build-v2.ts
+++ scripts/build-v2.ts
@@ -10,10 +10,10 @@ const viaAPIVersionV2 = '0.1.2';
 
 export async function buildV2() {
   try {
-    console.log(getDefinitionsPath())
-    console.log(getOutputPath())
+    console.log(getDefinitionsPath());
+    console.log(getOutputPath());
     const paths = glob.sync(getDefinitionsPath(), {absolute: true});
-    console.log(path.resolve('./'))
+    console.log(path.resolve('./'));
 
     const [v2Definitions] = [paths].map((paths) =>
       paths.map((f) => require(f))
@@ -27,10 +27,14 @@ export async function buildV2() {
         .map(keyboardDefinitionV2ToVIADefinitionV2)
         .reduce((p, n) => {
           if (n.vendorProductId in p) {
-            throw new Error('Duplicate id found);
+            console.log(
+              `Duplicate id found: ${n.name} collides with ${
+                p[n.vendorProductId].name
+              }`
+            );
           }
-          return {...p, [n.vendorProductId]: n}
-        }, {}),
+          return {...p, [n.vendorProductId]: n};
+        }, {} as any),
     };
 
     const outputPath = getOutputPath();
