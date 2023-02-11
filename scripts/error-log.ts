9798dc58382f1b185ffaa96ba7f160bbb6f5441d
Valdis Thomann <valdis.thomann@gmail.com>
2023-02-11 19:07:59 +1100

    Collect errors for all keyboards instead of terminating on first

--- scripts/error-log.ts
+++ scripts/error-log.ts
@@ -1,9 +1,28 @@
 import fs from 'fs/promises';
 
-export const clearErrorLog = async () => {
-  return fs.rm('build-error.log', {force: true});
-};
-export const writeToErrorLog = async (err: Error) => {
-  const errorStr = err.toString();
-  return fs.writeFile('build-error.log', errorStr);
-};
+export class ErrorLogger {
+  private _errors: Error[] = [];
+
+  public logError = (err: Error) => {
+    this._errors.push(err);
+  };
+
+  public getErrors = () => this._errors;
+
+  public writeErrorsToConsole = () => {
+    this._errors.forEach((err) => {
+      console.error(err);
+    });
+  };
+
+  public clearLogFile = async () => {
+    return fs.rm('build-error.log', {force: true});
+  };
+
+  public writeErrorsToLogFile = async () => {
+    this._errors.forEach(async (err) => {
+      const errorStr = err.toString();
+      return fs.writeFile('build-error.log', errorStr);
+    });
+  };
+}
